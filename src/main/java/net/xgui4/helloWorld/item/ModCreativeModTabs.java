8652207247d281faf8bcfa835be45009cf761be1
Xgui4 Studio <xgui2004@gmail.com>
2023-12-28 00:39:39 -0500

    Grosse mise à jour des ajout d'ordinateurs (pour le moment se sont seulement des commandes blocks)

--- src/main/java/net/xgui4/helloWorld/item/ModCreativeModTabs.java
+++ src/main/java/net/xgui4/helloWorld/item/ModCreativeModTabs.java
@@ -15,17 +15,15 @@ import net.xgui4.helloWorld.item.ModItems;
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
-
-    public static final RegistryObject<CreativeModeTab> HELLOWORD_TAB = CREATIVE_MODE_TABS.register("helloworld_tab",
+    public static final RegistryObject<CreativeModeTab> SAPHIRRE_TAB = CREATIVE_MODE_TABS.register("saphirre_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
                     .title(Component.translatable("creativetab.helloworld_tab"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
-                        pOutput.accept(ModItems.JAVA_CUP.get());
-                        pOutput.accept(ModItems.JAVA_SEED.get());
+
                         pOutput.accept(ModItems.METAL_DETECTOR.get());
-                        pOutput.accept(ModItems.TERMINAL.get());
+
                         pOutput.accept(ModItems.STRAWBERRY.get());
                         pOutput.accept(ModItems.PINE_CONE.get());
 
@@ -35,6 +33,16 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> COMPUTERS_TAB = CREATIVE_MODE_TABS.register("helloworld_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.COMMAND_BLOCK))
+                    .title(Component.translatable("computer.helloworld_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.JAVA_CUP.get());
+                        pOutput.accept(ModItems.JAVA_SEED.get());
+                        pOutput.accept(ModItems.TERMINAL.get());
                         pOutput.accept(ModBlocks.SOUND_BLOCK.get());
                         pOutput.accept(ModBlocks.JAVA_BLOCK.get());
 
@@ -45,7 +53,6 @@ public class ModCreativeModTabs {
                         pOutput.accept(Items.STRUCTURE_VOID);
                     })
                     .build());
-
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
     }
